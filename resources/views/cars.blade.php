6664ffaf2928569951a8a4558d73f4c966799a4b
CleberL08 <cleberlucas123@hotmail.com>
2020-09-12 22:47:39 -0300

    Inserting map on Cars tab

--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -2,26 +2,30 @@
 
 <!-- <a type="button" href="{{route('Home')}}" class="">Voltar</a> -->
 
-<nav class="spot">
 
+<nav class="spot">
+    
 <img src="./imgs/imgPark2.png" width="100%" height="20%">
 <ul id="nav">
     <li><a  href="{{route ('ShowCar')}}" ><i class="fa fa-home w3-xxlarge"></i><br>Inicio</a></li>
     <li><a  href"" ><i class="fa fa-user w3-xxlarge"></i><br>Clientes</a>
         <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Hospedagem</a></li>
-            <li><a href="#">SEO</a></li>
-            <li><a href="#">Sistemas</a></li>
+            <li><a href="#">Cadastrar cliente</a></li>
+            <li><a href="#">Ver clientes</a></li>
+            <li><a href="#">Data de pagamento</a></li>
+            
           </ul>
         </li>
-    <li><a href="{{route('Home')}}" ><i class="fa fa-exit w3-xxlarge"></i><br>Sair</a>  
-      
-    </li>
+        <li><a id ="closeButton "href="{{route('Home')}}" ><i  class ="fa fa-close   w3-xxlarge"></i><br>Sair</a></li>
+   
 <!-- 
-    <li><a href="#">Contato</a></li>
--->  </ul>
+    <li><a href="#">Contato</a></li>-->
+</ul>
+
 </nav>
+<div class="mapa">
+    <img src=".\imgs\carsSpots\parking.png" width="100px;">
+</div>
 
 
 
