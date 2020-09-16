638547ad7d8ef71281efc53c011e5662855fa9f6
CleberL08 <cleberlucas123@hotmail.com>
2020-09-15 22:10:25 -0300

    trying to solve form problem

--- resources/views/menuLayout.blade.php
+++ resources/views/menuLayout.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 <nav class="spot">
-    <!-- <a type="button" href="{{route('Home')}}" class="">Voltar</a> -->
+  
+   
     <img src="../imgs/imgPark2.png" style="width:100%; height:12vh;">
     <ul id="nav">
         <li><a  href="{{route ('ShowCar')}}" ><i class="fa fa-home w3-xxlarge"></i><br>Inicio</a></li>
--- resources/views/registerUser.blade.php
+++ resources/views/registerUser.blade.php
@@ -1,3 +1,41 @@
-@extends ('menulayout')
+@include('menuLayout')
+<body>  
+<div class="merda">
 
+        <div class ="registerUser">
+            <h1 class="w3-xxlarge ">Cadastro de clientes</h1>
+            <hr style="width: 80%">
+                 <form method="POST" action="">
+                    <h3 class="w3-xlarge leftTitle escuro">Dados do cliente:</h3><br><br>    
+                        <div class ="form-group esquerda" >
+                            <label>Nome:</label>
+                            <input class="form-control " type="text" placeholder="Insira o nome" required>
+                            <br>
+                            <label>Data de Vencimento:</label>
+                            <input class="form-control" id="date" type="date">
+                        </div>
+                            <br><br> 
+                    <h3 class="w3-xlarge leftTitle escuro aolado ">Dados do carro:</h3><br><br> 
+                        <div class ="form-group esquerda aolado">
+                            <label>Placa</label>
+                            <input class="form-control" type="text" placeholder= "Informe a placa do veiculo"  >
+                            <br>
+                            <label>Cor</label>
+                            <input class="form-control" type="text" placeholder="Informe a cor do veiculo" >
+                            <br>
+                            <label>Marca</label>
+                            <input class="form-control" type="text" placeholder="Informe a Marca do veiculo"  >
+                            <br>
+                            <label>Modelo</label>
+                            <input class="form-control" type="text" placeholder="Informe o Modelo do veiculo"  >
+                                                       
+
+                        </div>
+                        <br>
+                    <br><br><br>
+                </form>   
+        </div>
+    </div>
+
+</body>
 
